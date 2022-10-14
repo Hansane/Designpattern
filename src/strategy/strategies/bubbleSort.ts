6925befb435c6bb7e79ddedfe8167b254c0146ba
Johannes Kerzenmacher <jkerzenmacher@neusta.de>
2022-10-14 11:17:43 +0200

    get bubble inserted

--- src/strategy/strategies/bubbleSort.ts
+++ src/strategy/strategies/bubbleSort.ts
@@ -1,11 +1,24 @@
-import { ISortingStrategy } from "../sortingStrategy";
+import {ISortingStrategy} from "../sortingStrategy";
 
 export class BubbleSort implements ISortingStrategy {
 	public get sortName() {
 		return "BubbleSort";
 	}
 
-	public sort(numbers: number[]): number[] {
-		return numbers;
-	}
+    public sort(numbers: number[]): number[]{
+        let swapped = false;
+
+        for(let i = 0; i < numbers.length; i++) {
+            if(i >= numbers.length -1) break;
+            if(numbers[i] > numbers[i+1]) {
+                let num = numbers[i+1];
+                numbers[i+1] = numbers[i];
+                numbers[i] = num;
+                swapped = true;
+            }
+        }
+
+        if(!swapped) return numbers;
+        return this.sort(numbers);
+    }
 }
--- src/strategy/strategies/insertionSort.ts
+++ src/strategy/strategies/insertionSort.ts
@@ -1,11 +1,31 @@
-import { ISortingStrategy } from "../sortingStrategy";
+import {ISortingStrategy} from "../sortingStrategy";
 
 export class InsertionSort implements ISortingStrategy {
 	public get sortName() {
 		return "InsertionSort";
 	}
 
-	public sort(numbers: number[]): number[] {
-		return numbers;
-	}
+    public sort(numbers: number[]): number[]{
+        let result = [];
+        result.push(numbers[0])
+        for(let i = 0; i < numbers.length; i++) {
+            if(i == 0) {
+
+                continue;
+            }
+            let inserted = false;
+            for(let j = 0; j < result.length; j++) {
+                if(numbers[i] < result[j]) {
+                    console.log(result);
+                    result.splice(j, 0, numbers[i]);
+                    inserted = true;
+                    break;
+                }
+            }
+            if(!inserted) {
+                result.push(numbers[i]);
+            }
+        }
+        return result;
+    }
 }
