dbd49cac1256eab3fd75c116c3f9f2fd6dc54a1e
DatPriest <lcwiertnia@web.de>
2022-10-14 11:27:37 +0200

    SelectionSort Yes with Z

--- src/strategy/strategies/selectionSort.ts
+++ src/strategy/strategies/selectionSort.ts
@@ -1,11 +1,33 @@
-import { ISortingStrategy } from "../sortingStrategy";
+import {ISortingStrategy} from "../sortingStrategy";
 
 export class SelectionSort implements ISortingStrategy {
 	public get sortName() {
 		return "SelectionSort";
 	}
 
-	public sort(numbers: number[]): number[] {
-		return numbers;
-	}
+    public sort(numbers: number[]): number[] {
+
+        for (let j = 0; j < numbers.length; j++) {
+            let highestValue;
+            let highestIndex;
+            let tempValue;
+            for (let i = j; i < numbers.length; i++) {
+                const actualNumber = numbers[i]
+                if (highestValue === undefined) {
+                    highestValue = actualNumber;
+                    highestIndex = i;
+                }
+                else if (actualNumber > highestValue) {
+                    highestValue = actualNumber;
+                    highestIndex = i;
+                }
+            }
+            if (highestIndex != j) {
+                tempValue = [numbers[j], j,  numbers[highestIndex], highestIndex];
+                numbers[j] = numbers[highestIndex];
+                numbers[highestIndex] = tempValue[0];
+            }
+        }
+        return numbers;
+    }
 }
